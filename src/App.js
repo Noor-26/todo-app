5ca8e19e9f3670434ef9f3ae0de6a8e37e4a5bef
Noor-26 <nk26982698@gmail.com>
2022-06-30 20:49:20 +0600

    navber created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home/Home';
+import Navber from './Components/Navber/Navber';
 
 function App() {
   return (
     <div className="App">
+      <Navber/>
       <Routes>
         <Route path='/' element={<Home/>}/>
       </Routes>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,32 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+  themes: [
+    { 
+      
+      mytheme: {
+      
+"primary": "#3bd3ce",
+      
+"secondary": "#dbba67",
+      
+"accent": "#860fc6",
+      
+"neutral": "#2E2A37",
+      
+"base-100": "#37393E",
+      
+"info": "#A0C0F3",
+      
+"success": "#73DEB5",
+      
+"warning": "#F7AC4A",
+      
+"error": "#EF1A3A",
+      },
+    },
+  ],
+},
   plugins: [require("daisyui")],
 }
